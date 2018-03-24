f92d4e956c5fbe1e2ef5a80ba73d08d42e2020fd
Jonatan Pedersen <jp@jonatanpedersen.com>
2018-03-24 18:02:14 +0100

    fixes env

--- packages/@jambon/router/src/env.ts
+++ packages/@jambon/router/src/env.ts
@@ -2,6 +2,6 @@ import { match, AsyncReducerFunction, HttpContext } from '@jambon/core';
 
 export function env (name : string, value : string, ...reducers : AsyncReducerFunction[]) : AsyncReducerFunction {
 	return async function env (context : HttpContext) : Promise<HttpContext> {
-		return match(async context => process.env[name] === value)(context);
+		return match(async context => process.env[name] === value, ...reducers)(context);
 	}
 }
